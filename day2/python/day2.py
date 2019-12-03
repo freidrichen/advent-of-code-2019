f78e32aafa4c1cbd5ed201de1b2739b20fe4ef87
Magnus Sandén <thefreid@gmail.com>
2019-12-03 07:15:04 +0100

    Fix incorrect number of arguments

--- day2/python/day2.py
+++ day2/python/day2.py
@@ -16,19 +16,23 @@ while True:
     if op_code == 1:
         # Add
         arg_count = 3
-        input_addr1, input_addr2, output_addr = memory[instruction_ptr + 1 : instruction_ptr + arg_count]
+        args = memory[instruction_ptr + 1 : instruction_ptr + arg_count + 1]
+        input_addr1, input_addr2, output_addr = args
         memory[output_addr] = memory[input_addr1] + memory[input_addr2]
         instruction_ptr += 1 + arg_count
     elif op_code == 2:
         # Multiply
         arg_count = 3
-        input_addr1, input_addr2, output_addr = memory[instruction_ptr + 1 : instruction_ptr + arg_count]
+        args = memory[instruction_ptr + 1 : instruction_ptr + arg_count + 1]
+        input_addr1, input_addr2, output_addr = args
         memory[output_addr] = memory[input_addr1] * memory[input_addr2]
         instruction_ptr += 1 + arg_count
     elif op_code == 99:
         # Halt
         arg_count = 0
         break
+    else:
+        raise ValueError(f"Unknown op code: {op_code}")
 
 print(f"Memory[0] (part 1): {memory[0]}")
 
